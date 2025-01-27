e1b5c0143d215812c84c4d8852125b0ebb727804
yuni0725 <yuni0725@naver.com>
2025-01-27 21:08:59 +0900

    Day 8 Challenge

--- tweets/admin.py
+++ tweets/admin.py
@@ -4,9 +4,20 @@ from .models import Tweet, Like
 
 @admin.register(Tweet)
 class TweetAdmin(admin.ModelAdmin):
-    list_display = ("__str__", "user", "created_at", "updated_at")
+    list_display = (
+        "__str__",
+        "user",
+        "count_like",
+        "created_at",
+        "updated_at",
+    )
 
 
 @admin.register(Like)
 class LikeAdmin(admin.ModelAdmin):
-    list_display = ("__str__", "user", "created_at", "updated_at")
+    list_display = (
+        "__str__",
+        "user",
+        "created_at",
+        "updated_at",
+    )
--- tweets/models.py
+++ tweets/models.py
@@ -4,15 +4,30 @@ from common.models import CommonModel
 
 class Tweet(CommonModel):
     payload = models.CharField(max_length=180)
-    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="tweet",
+    )
 
     def __str__(self):
-        return f"Tweet by {self.user}"
+        return f"{self.payload}"
+
+    def count_like(self):
+        return self.like.count()
 
 
 class Like(CommonModel):
-    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
-    tweet = models.ForeignKey("tweets.Tweet", on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        "users.User",
+        on_delete=models.CASCADE,
+        related_name="like",
+    )
+    tweet = models.ForeignKey(
+        "tweets.Tweet",
+        on_delete=models.CASCADE,
+        related_name="like",
+    )
 
     def __str__(self):
         return f"Like of {self.tweet}"
