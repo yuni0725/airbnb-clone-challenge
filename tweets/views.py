1c2749143a096e95b75292cf7ac0f4db28e86b82
yuni0725 <yuni0725@naver.com>
2025-01-29 19:01:26 +0900

    Day 10 Challenge

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Tweet
 
-# Create your views here.
+
+def see_all_tweet(request):
+    tweets = Tweet.objects.all()
+    return render(request, "all_room.html", {"tweets": tweets})
